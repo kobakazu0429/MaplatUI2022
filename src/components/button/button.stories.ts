ded341b8a073eefe113db823937c447110cbecd4
kobakazu0429 <kobakazu10@gmail.com>
2022-06-23 00:00:31 +0900

    add icons

--- src/components/button/button.stories.ts
+++ src/components/button/button.stories.ts
@@ -1,5 +1,6 @@
 import { Meta, Story } from "@storybook/html";
 import button from "./index.scss";
+import icons, { type Styles, type ClassNames } from "../../icons.scss";
 
 export default {
   title: "Button",
@@ -19,6 +20,12 @@ const createButton = ({ label, classNames = [] }: ButtonProps) => {
   btn.innerText = label;
   btn.className = classNames.join(" ");
   return btn;
+};
+
+const createIcon = (icon: Styles[ClassNames]) => {
+  const i = document.createElement("i");
+  i.classList.add(icon);
+  return i;
 };
 
 const Template: Story<ButtonProps> = (args) => {
@@ -43,15 +50,22 @@ RectButton.args = {
   classNames: [button["rect-button"]],
 };
 
-export const SquareButton = Template.bind({});
-SquareButton.args = {
-  label: "+",
-  classNames: [button["square-button"]],
+export const SquareButton: Story = () => {
+  const btn = document.createElement("button");
+  btn.type = "button";
+  btn.className = button["square-button"];
+  btn.appendChild(createIcon(icons["i-gps_on-24"]));
+  return btn;
 };
 
 export const SquareButtonGroup: Story = () => {
-  const top = createButton({ label: "+" });
-  const bottom = createButton({ label: "-" });
+  const top = document.createElement("button");
+  const bottom = document.createElement("button");
+  top.type = "button";
+  bottom.type = "button";
+
+  top.appendChild(createIcon(icons["i-zoom_in"]));
+  bottom.appendChild(createIcon(icons["i-zoom_out"]));
 
   const wrapper = document.createElement("div");
   wrapper.appendChild(top);
