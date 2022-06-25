66401ec8d4ab449202e1ca2a4b6ee9c4e9f63b9c
kobakazu0429 <kobakazu10@gmail.com>
2022-06-25 13:10:26 +0900

    fix: mkdir dist if not exsit

--- bin/build-css.mjs
+++ bin/build-css.mjs
@@ -8,9 +8,9 @@ import removeExportPlugin from "../postcssPlugins/removeExportPlugin.js";
 import convoluteSelectorsPlugin from "../postcssPlugins/convoluteSelectorsPlugin.js";
 
 const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
-const __dirname = path.dirname(new URL(import.meta.url).pathname);
+
 const maplatScssPath = path.resolve(path.join(__dirname, "..", "src", "maplat.scss"));
-const distPath = path.resolve(path.join(__dirname, "..", "dist", "maplat.css"));
+const distPath = path.resolve(path.join(__dirname, "..", "dist"));
 
 const postcssProcess = async (css) => {
   const result = await postcss([removeExportPlugin, convoluteSelectorsPlugin]).process(css);
@@ -19,11 +19,13 @@ const postcssProcess = async (css) => {
 
 async function main() {
   try {
+    await fs.mkdir(distPath, { recursive: true });
+
     let css = sass.compile(maplatScssPath).css;
     css = await postcssProcess(css);
     css = new CleanCSS({ level: 2 }).minify(css).styles;
 
-    await fs.writeFile(distPath, css);
+    await fs.writeFile(path.join(distPath, "maplat.css"), css);
 
     console.log("Build done.");
   } catch (error) {
