ad55a013a51ac27affa756f8516428b016d77634
kobakazu0429 <kobakazu10@gmail.com>
2022-07-06 14:01:25 +0900

    feat: layer switcher base

--- src/components/layer-switcher/layer-switcher.stories.ts
+++ src/components/layer-switcher/layer-switcher.stories.ts
@@ -1,44 +1,77 @@
 import { Meta, Story } from "@storybook/html";
 import { useScript } from "./../../js/useScript";
-import Swiper, { Navigation, Keyboard } from "swiper";
+import Swiper, { Navigation, Keyboard, Mousewheel } from "swiper";
 import style from "../../maplat.scss";
 
 export default {
   title: "LayerSwitcher",
 } as Meta;
 
+const createSlide = () => {
+  return `
+  <div class="${style["swiper-slide"]} ${style["layer-slide"]}">
+    <div class="${style["layer-infomation"]}">
+      <i class="${style["i-time-24"]}"></i>
+      明治期 / 1868年以降
+    </div>
+    <div class="${style["layer-slide-img-wrapper"]}">
+      <img src="/Nara,_Japan_(709-784).jpeg"></img>
+    </div>
+    <div class="${style["layer-name"]}">
+      奈良市鳥瞰図
+    </div>
+  </div>
+  `;
+};
+
 export const LayerSwitcher: Story = () => {
   const div = document.createElement("div");
-  div.innerHTML = `
-  <div class="${style["swiper"]} ${style["maplat-swiper"]}">
-    <div class="${style["swiper-wrapper"]} style="height:100%;">
-      <div class="${style["swiper-slide"]} ${style["maplat-slide"]}">Slide 1</div>
-      <div class="${style["swiper-slide"]} ${style["maplat-slide"]}">Slide 2</div>
-      <div class="${style["swiper-slide"]} ${style["maplat-slide"]}">Slide 3</div>
-      <div class="${style["swiper-slide"]} ${style["maplat-slide"]}">Slide 4</div>
-      <div class="${style["swiper-slide"]} ${style["maplat-slide"]}">Slide 5</div>
-      <div class="${style["swiper-slide"]} ${style["maplat-slide"]}">Slide 6</div>
-      <div class="${style["swiper-slide"]} ${style["maplat-slide"]}">Slide 7</div>
-      <div class="${style["swiper-slide"]} ${style["maplat-slide"]}">Slide 8</div>
-      <div class="${style["swiper-slide"]} ${style["maplat-slide"]}">Slide 9</div>
-      <div class="${style["swiper-slide"]} ${style["maplat-slide"]}">Slide 10</div>
-    </div>
+  div.classList.add(style["layer-switcher"]);
+  div.style.height = "200px";
 
-    <!-- Add Arrows -->
-    <div class="${style["swiper-button-next"]}"></div>
-    <div class="${style["swiper-button-prev"]}"></div>
+  div.innerHTML = `
+  <div class="${style["swiper"]} ${style["layer-switcher-swiper"]}">
+    <div class="${style["swiper-wrapper"]}">
+      ${createSlide()}
+      ${createSlide()}
+      ${createSlide()}
+      ${createSlide()}
+      ${createSlide()}
+      ${createSlide()}
+      ${createSlide()}
+      ${createSlide()}
+      ${createSlide()}
+      ${createSlide()}
+      ${createSlide()}
+      ${createSlide()}
+      ${createSlide()}
+    </div>
+  </div>
+  <div class="${style["layer-switcher-prev-button"]}">
+    <i class="${style["i-arrow-02-prev-36"]}"></i>
+  </div>
+  <div class="${style["layer-switcher-next-button"]}">
+    <i class="${style["i-arrow-02-next-36"]}"></i>
   </div>
 `;
 
   useScript(() => {
     const swiper = new Swiper("." + style["swiper"], {
-      modules: [Navigation, Keyboard],
+      modules: [Navigation, Keyboard, Mousewheel],
       keyboard: {
         enabled: true,
       },
+      mousewheel: true,
+      loop: true,
+      slidesPerView: "auto",
+      spaceBetween: 10,
       navigation: {
-        nextEl: "." + style["swiper-button-next"],
-        prevEl: "." + style["swiper-button-prev"],
+        nextEl: "." + style["layer-switcher-next-button"],
+        prevEl: "." + style["layer-switcher-prev-button"],
+        // disabledClass: style["layer-switcher-navigation-disabled"],
+        // hiddenClass: style["layer-switcher-navigation-hidden"],
+        // lockClass: style["layer-switcher-navigation-lock"],
+        // navigationDisabledClass: style[""],
       },
       wrapperClass: style["swiper-wrapper"],
       slideActiveClass: style["swiper-slide-active"],
