7a5d7ef2b3e28aed6e88703ec946151222e36eb6
kobakazu0429 <kobakazu10@gmail.com>
2022-07-25 11:36:53 +0900

    fix: icon viewer

--- src/tokens/icons.stories.ts
+++ src/tokens/icons.stories.ts
@@ -4,36 +4,48 @@ import type { ClassNames } from "../icons/maplat-icons.scss";
 
 export default {
   title: "Tokens/Icons",
+  parameters: {
+    layout: "fullscreen",
+  },
 } as Meta;
 
 const createIcon = (iconName: ClassNames) => {
   const wrapper = document.createElement("div");
   wrapper.style.display = "flex";
   wrapper.style.height = "100px";
-  wrapper.style.width = "300px";
-
-  const span = document.createElement("span");
-  span.innerText = iconName;
-  span.style.width = "200px";
-  wrapper.appendChild(span);
+  wrapper.style.width = "200px";
+  wrapper.style.flexDirection = "row";
+  wrapper.style.justifyContent = "center";
+  wrapper.style.alignItems = "center";
+  wrapper.style.textAlign = "center";
 
   const wrapper2 = document.createElement("div");
   wrapper2.style.height = "100px";
   wrapper2.style.width = "100px";
   wrapper2.style.fontSize = "80px";
+  wrapper2.style.textAlign = "center";
   const i = document.createElement("i");
   i.classList.add(icons[iconName]);
-
   wrapper2.appendChild(i);
+
+  const span = document.createElement("span");
+  span.innerText = iconName;
+  span.style.width = "100%";
+
   wrapper.appendChild(wrapper2);
+  wrapper.appendChild(span);
+
   return wrapper;
 };
 
 export const Icons: Story = () => {
   const wrapper = document.createElement("div");
   wrapper.style.display = "flex";
-  wrapper.style.flexDirection = "column";
   wrapper.style.gap = "10px";
+  wrapper.style.flexDirection = "column";
+  wrapper.style.alignItems = "left";
+  wrapper.style.overflowY = "scroll";
+  wrapper.style.height = "100vh";
 
   (Object.keys(icons) as ClassNames[]).forEach((iconName) => {
     wrapper.appendChild(createIcon(iconName));
